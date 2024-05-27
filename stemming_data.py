d33419397774f1d83f12014c2aa75cae8f326b2b
Paola-CansecoPenagos <paolaacp16@gmail.com>
2024-05-27 11:09:56 -0600

    update analisis

--- stemming_data.py
+++ stemming_data.py
@@ -22,4 +22,26 @@ text_columns = data.select_dtypes(include=['object']).columns
 for col in text_columns:
     data[col] = data[col].apply(apply_lemmatization)
 
+data['Si tuvieras una aplicación para registrar todas tus actividades, ¿para qué ámbitos la usarías?'] = \
+    data['Si tuvieras una aplicación para registrar todas tus actividades, ¿para qué ámbitos la usarías?'].replace(
+        ['Todas mis actividades cotidianas', 'Para todo ambito'], 
+        'Para todo ambito'
+    )
+
+data['¿En cual de las siguientes opciones prefieres anotar tus actividades?'] = \
+    data['¿En cual de las siguientes opciones prefieres anotar tus actividades?'].replace(
+        'Programas que utilizan la sincronización de datos tanto en el teléfono como en el programa de escritorio', 
+        'Programa para celular'
+    )
+
+def update_color(row):
+    if row == 'gris si no se a iniciado y rojo si no fue hecha':
+        color_counts = data['¿Con qué color asocias más que una tarea está sin iniciar o no fue hecha?'].value_counts()
+        most_common_color = color_counts.idxmax()
+        return most_common_color
+    return row
+
+data['¿Con qué color asocias más que una tarea está sin iniciar o no fue hecha?'] = \
+    data['¿Con qué color asocias más que una tarea está sin iniciar o no fue hecha?'].apply(update_color)
+
 data.to_csv('Lemmatized_TaskMaster.csv', index=False)
